0e0a06860cc4e5634e98b4a7a0f05aa29ed5d47c
latticet@gmail.com <935948757@qq.com>
2022-11-30 11:48:14 +0800

    数据类型转换

---
+++ day2/02-数据类型转换.py
@@ -0,0 +1,41 @@
+# TODO int()
+# 将其他数据类型转化为int
+# str 可以转化为字符串数字
+str1 = '123'
+num1 = int(str1)
+print(type(num1))
+
+# str2 = 'a123'
+# num2 = int(str2)
+
+# float 可以转化
+f1 = 10.2
+num3 = int(f1)
+print(num3)
+print(type(num3))
+
+# boolean
+bool1 = True
+print(int(bool1))
+bool2 = False
+print(int(bool2))
+print('==' * 20)
+
+# TODO float()
+# 将其他数据类型转化为float
+# str  数字型字符串  ‘123’ ‘10.1’
+str1 = '123'
+print(float(str1))
+str2 = '10.1'
+print(float(str2))
+str3 = 'a10.1'
+# print(float(str3))
+
+# int
+num1 = 10
+print(float(num1))
+
+# float
+print(float(bool1))
+print(float(bool2))
+
