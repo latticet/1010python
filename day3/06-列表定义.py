fe5c9e6d6cdd759d2657ea47d7438d884a0837e7
latticet@gmail.com <935948757@qq.com>
2022-12-02 14:40:09 +0800

    列表定义

---
+++ day3/06-列表定义.py
@@ -0,0 +1,34 @@
+# 数据类型
+"""
+数字 int float
+str
+bool
+None
+list 列表
+tuple 元组
+dict 字典
+set 集合
+"""
+
+# TODO 列表定义
+# 说明：
+"""
+1. 列表中的数据叫做列表元素
+2. 列表中可以存放任何数据类型
+3. 列表中的元素是可以重复的
+"""
+# 第一种:[常用]
+# 空列表
+list1 = []
+# 有数据的列表
+list2 = ['python', 'mysql', 1, True, 12.1, None, 'python', 1]
+print(list2)
+
+# 第二种:
+# 空列表
+list3 = list()
+# 有数据的列表
+list4 = list('hello')
+
+print(list4)
+
