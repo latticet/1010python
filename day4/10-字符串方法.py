0d81f5efddd9f0c9aceb5dbaff9c2a69c76a9284
latticet@gmail.com <935948757@qq.com>
2022-12-03 14:20:39 +0800

    字符串方法

--- day4/10-字符串方法.py
+++ day4/10-字符串方法.py
@@ -37,15 +37,72 @@ print(str3.title())
 """
 
 # TODO 判断开头或结尾
+"""
 str1 = 'hello'
 # str.startswith('字符串')   是否以某字符串开始
 print(str1.startswith('he'))
 print(str1.startswith('ab'))
 print('--' * 20)
 
-
 # str.endswith('字符串')   是否以某字符串结尾
 print(str1.endswith('llo'))
 print(str1.endswith('llb'))
+print('==' * 20)
+"""
+
+# TODO 字符串格式化
+# str.format()   字符串格式化函数
+"""
+name = 'hello'
+age = 18
+# 第一种 默认顺序
+str1 = '姓名：{};年龄：{}'.format(name, age)
+print(str1)
+# 第二种 指定顺序
+str1 = '姓名：{0};年龄：{1}'.format(name, age)
+print(str1)
+# 第三种 指定名字
+str1 = '姓名：{name};年龄：{age}'.format(name=name, age=age)
+str2 = '姓名：{name};年龄：{age}'.format(name='good', age=20)
+print(str1)
+print(str2)
+"""
+
+# TODO 字符串拼接
+# str.join(list)  列表转字符串
+"""
+list1 = ['python', 'mysql', 'linux']
+str1 = '-'.join(list1)
+str2 = ','.join(list1)
+str3 = ' '.join(list1)
+str4 = ''.join(list1)
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+"""
+
+# TODO 字符串替换
+# str.replace(原内容，新内容)  全部替换
+"""
+str1 = 'hello python, 你好 python'
+str2 = str1.replace('python', 'mysql')
+print(str2)
+"""
+
+# TODO 判断字符串构成
+# 判断字符串由数字组成 str.isdigit()
+print('123'.isdigit())
+print('a23'.isdigit())
+print('--' * 20)
+# 判断字符串由字母组成 str.isalpha()
+print('abc'.isalpha())
+print('a23'.isalpha())
+print('123'.isalpha())
+print('--' * 20)
+# 判断字符串由字母或数字组成
+print('abc'.isalnum())
+print('a23'.isalnum())
+print('123'.isalnum())
 
 
