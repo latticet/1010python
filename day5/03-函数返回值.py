9dbc43f8d5a63c0268f2dae84737df5cdd0d39d8
latticet@gmail.com <935948757@qq.com>
2022-12-05 11:11:52 +0800

    函数返回值

---
+++ day5/03-函数返回值.py
@@ -0,0 +1,23 @@
+# TODO 函数返回
+"""
+语法：
+def fn():
+    return 表达式
+说明：
+1. 函数返回运行结果：return返回表达式运行的结果
+2. 返回到函数调用的地方
+3. 退出循环：return后面的代码不执行
+"""
+
+
+# 函数定义
+def add(a, b):
+    result = a + b
+    return result
+    print('hello')  # return 后面的代码不执行
+
+# 函数调用
+# add(1, 2)
+# print(add(1, 3))
+# total = add(3, 5)
+# print(total)
