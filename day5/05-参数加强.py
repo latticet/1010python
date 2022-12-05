85881cfeb53ecaac9a7ecf60947be074ec2852e5
latticet@gmail.com <935948757@qq.com>
2022-12-05 11:27:44 +0800

    函数加强

---
+++ day5/05-参数加强.py
@@ -0,0 +1,25 @@
+# 定义函数时的参数
+# TODO 必填参数（必须参数）
+# 必填参数在函数调用的时候，必须传入对应的实参
+def fn1(a, b):
+    print(a, b)
+
+
+fn1(1, 2)
+print('==' * 20)
+
+# TODO 默认参数
+# 默认参数，在函数调用的时候，可以不用传入实参。使用默认值
+"""
+说明：
+默认参数，必须在必填参数的后面
+"""
+
+
+def add_stu(name, age, class_name='1010'):
+    print(name, age, class_name)
+
+
+add_stu('hello', 16)
+add_stu('good', 18)
+add_stu('apple', 17, '1111')
