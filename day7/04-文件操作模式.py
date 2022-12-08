8dde16b6d3f1add97cc362a2e8e789f0d425141e
latticet@gmail.com <935948757@qq.com>
2022-12-08 14:22:11 +0800

    文件操作模式

--- day7/04-文件操作模式.py
+++ day7/04-文件操作模式.py
@@ -18,10 +18,67 @@ f.close()
 """
 
 # TODO b模式
+"""
 # 打开文件
 f = open('resource/demo4.txt', 'wb')
 # 操作文件
 bytes_str = '555'.encode()
 f.write(bytes_str)
+# 关闭文件
+f.close()
+"""
+
+# TODO +模式
+"""
+# 打开文件
+f = open('resource/demo4.txt', 'r+', encoding='utf8')
+# 操作文件
+# 读取内容
+content = f.read()
+print(content)
+# 写入内容
+f.write('\n777')
+# 关闭文件
+f.close()
+"""
+"""
+# 打开文件
+f = open('resource/demo4.txt', 'w+', encoding='utf8')
+# 操作文件
+# 读取内容
+content = f.read()
+print(content)
+# 写入内容
+f.write('\n777')
+# 关闭文件
+f.close()
+"""
+
+"""
+# 打开文件
+f = open('resource/demo4.txt', 'a+', encoding='utf8')
+# 操作文件
+# 读取内容
+content = f.read()
+print(content)
+# 写入内容
+f.write('\n777')
+# 关闭文件
+f.close()
+"""
+
+f = open('resource/demo5.txt', 'w+', encoding='utf8')
+# 操作文件
+# 写入内容
+f.write('你好')
+
+# 移动光标到最左边
+# 语法：f.seek(size)
+f.seek(3)
+
+# 读取内容
+content = f.read()
+print(content)
+
 # 关闭文件
 f.close()
