278c786953e7305392d55e543219966f5ba32bd4
latticet@gmail.com <935948757@qq.com>
2022-12-11 10:34:52 +0800

    继承方式-多继承

---
+++ day9/03-继承方式-多继承.py
@@ -0,0 +1,23 @@
+"""
+一个类同时继承多个父类
+"""
+
+
+class A:
+    def info_a(self):
+        print('a')
+
+
+class B:
+    def info_b(self):
+        print('b')
+
+
+# C类继承了A类和B类
+class C(A, B):
+    pass
+
+
+c = C()
+c.info_a()
+c.info_b()
