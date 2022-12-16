5e75951c82b717e4204f968bb2601058b6d91d92
zhangyaowen <zhangyaowenjy@163.com>
2022-12-16 15:12:19 +0800

    模块导入时发生的事儿

---
+++ day11/04-模块导入时/module1.py
@@ -0,0 +1,15 @@
+# 变量
+name = 'module1'
+
+
+# 函数
+def fn():
+    print('module1-fn函数执行')
+
+
+# 类
+class Demo:
+    pass
+
+
+print('module1被执行了')
---
+++ day11/04-模块导入时/导入模块.py
@@ -0,0 +1,3 @@
+import module1
+
+import module1  # 重复导入不执行
