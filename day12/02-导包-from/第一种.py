b626b456b5adb599a2e4459685cb3c3d1b9284d4
zhangyaowen <zhangyaowenjy@163.com>
2022-12-17 09:57:02 +0800

    导包-from-第一种

--- day12/02-导包-from/第一种.py
+++ day12/02-导包-from/第一种.py
@@ -1,6 +1,17 @@
 # 语法： from 包[.包.n] import 模块1 [as 别名], 模块2 [as 别名], ...
 from package1 import module2, module1
+from package2.package_a import module_a1 as a1, module_a2 as a2
+from package2.package_b import module_b1, module_b2
 
 print(module1.name)
-
 print(module2.name)
+
+print('==' * 20)
+
+print(a1.name)
+print(a2.name)
+
+print('==' * 20)
+
+print(module_b2.name)
+print(module_b1.name)
