8603c09fc83f38edbae208b2fead42ec43e1a666
latticet@gmail.com <935948757@qq.com>
2022-12-23 15:48:24 +0800

    标量子查询， 列子查询

--- mysql/day1/09-子查询.sql
+++ mysql/day1/09-子查询.sql
@@ -27,3 +27,20 @@ SELECT * FROM student WHERE class_id
 in (SELECT class_id FROM class WHERE class_name = '1班' 
 or class_name = '2班')
 
+-- 行子查询（子查询结果为一行）
+-- 查询class_id=1这个班级最大的人,年龄最大的人
+-- a. 查询class_id=1这个班最大的年龄
+SELECT class_id, max(stu_age) FROM student WHERE class_id = 1;
+-- b. 根据年龄和班级id查询学生信息
+SELECT * FROM student WHERE class_id = 1 and stu_age = 21;
+-- 子查询语句
+SELECT * FROM student WHERE (class_id ,stu_age) =
+(SELECT class_id, max(stu_age) FROM student WHERE class_id = 1);
+
+-- 表子查询（子查询结果为多行多列）
+-- a. 查询分数大于39的所有学生信息
+SELECT * FROM student WHERE stu_score > 39;
+-- b. 在分数大于39的学生里找出年龄小于30的学生信息
+SELECT * FROM
+(SELECT * FROM student WHERE stu_score > 39) student_score_gt_39
+WHERE stu_age < 30;
