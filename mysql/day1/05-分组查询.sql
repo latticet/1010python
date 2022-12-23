c08234d936bd8387122fd47e9b0dbc8c9e3236bf
latticet@gmail.com <935948757@qq.com>
2022-12-23 10:56:39 +0800

    分组查询

--- mysql/day1/05-分组查询.sql
+++ mysql/day1/05-分组查询.sql
@@ -19,3 +19,17 @@ SELECT class_id, AVG(stu_age) FROM student GROUP BY class_id;
 -- 在分组后，用来聚合其他非分组字段
 -- 查询每个班级的学生姓名
 SELECT class_id, GROUP_CONCAT(stu_name) FROM student GROUP BY class_id;
+
+-- HAVING配合分组使用，不能单独使用
+-- 对分组之后的数据进行过滤
+-- 查询人数大于3的班级
+SELECT class_id FROM student GROUP BY class_id HAVING COUNT(*) > 3;
+
+-- 平均年龄大于30的班级
+SELECT class_id FROM student GROUP BY class_id HAVING AVG(stu_age) >30;
+
+-- where 和 group by同时使用
+-- 查询每个班年龄大于20的所有同学姓名
+SELECT class_id, GROUP_CONCAT(stu_name) FROM student
+WHERE stu_age > 20
+GROUP BY class_id;
