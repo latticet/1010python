40094040b356c59bd93b2399784afd43f4ea838d
latticet@gmail.com <935948757@qq.com>
2022-12-26 12:00:32 +0800

    三元运算符

---
+++ mysql/day3/db.py
@@ -0,0 +1,57 @@
+import pymysql
+
+"""
+类名： DB
+方法：
+    __init__:
+        数据库对象
+        游标对象
+    read: 查询
+    write：增删改
+    __del__:
+        关闭游标对象
+        关闭数据库对象
+"""
+
+
+class DB:
+    def __init__(self, user, password, database,
+                 host='localhost', port=3306,
+                 charset='utf8', cursor_type=pymysql.cursors.Cursor):
+        # 初始化数据库连接对象和游标对象
+        self.conn = pymysql.connect(host=host, port=port,
+                                    user=user, password=password,
+                                    database=database, charset=charset)
+        self.cursor = self.conn.cursor(cursor_type)
+        """
+        if cursor_type == pymysql.cursors.Cursor:
+            self.result_data = ()
+        else:
+            self.result_data = []
+        """
+        self.result_data = () if cursor_type == pymysql.cursors.Cursor else []
+
+    def read(self, sql, args=None):
+        """查询操作"""
+        try:
+            # 执行sql语句
+            rows = self.cursor.execute(sql, args=args)
+            # 获取结果集
+            data = self.cursor.fetchall()
+        except Exception as e:
+            return 0, self.result_data
+        else:
+            return rows, data
+
+    def write(self):
+        """写入操作"""
+        pass
+
+    def __del__(self):
+        self.cursor.close()
+        self.conn.close()
+
+
+if __name__ == '__main__':
+    db = DB('root', 'root', 'advanced')
+    print(db.read('select * from student1'))
