81feadba8a888e4fe62b7a8e28e2436fbf9b1742
latticet@gmail.com <935948757@qq.com>
2022-12-26 10:55:14 +0800

    sql注入解决

--- mysql/day3/03-sql注入问题.py
+++ mysql/day3/03-sql注入问题.py
@@ -7,6 +7,7 @@ name = input('学生姓名：')
 conn = pymysql.connect(user='root', password='root', database='advanced', charset='utf8')
 cursor = conn.cursor()
 # 执行sql语句
+# TODO sql注入问题
 sql = f'select * from student where stu_name = "{name}"'
 # select * from student where stu_name = "" or 1 = 1 or ""
 rows = cursor.execute(sql)
