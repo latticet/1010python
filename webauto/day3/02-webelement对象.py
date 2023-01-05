a9d3a291777db19f79eb512bab425221f697f23f
latticet@gmail.com <935948757@qq.com>
2023-01-05 11:35:02 +0800

    webelement对象

--- webauto/day3/02-webelement对象.py
+++ webauto/day3/02-webelement对象.py
@@ -1,3 +1,5 @@
+import os
+
 from selenium import webdriver
 import time
 
@@ -29,6 +31,62 @@ class TestCase:
         news_element = self.driver.find_element_by_link_text('新闻')
         print('a:', news_element.text)
 
+    def send_keys(self):
+        """在文本域中输入内容"""
+        # 定位输入框
+        kw = self.driver.find_element_by_id('kw')
+        # 输入内容
+        kw.send_keys('hello world')
+
+    def clear(self):
+        """清空文本域中的内容"""
+        time.sleep(1)
+        kw = self.driver.find_element_by_id('kw')
+        kw.clear()
+
+    def click(self):
+        """可以点击的标签 a input:button input:submit"""
+        self.driver.find_element_by_id('kw').send_keys('hello')
+        su = self.driver.find_element_by_id('su')
+        su.click()
+
+    def get_attribute(self):
+        """获取标签属性"""
+        kw = self.driver.find_element_by_id('kw')
+        print(kw.get_attribute('id'))
+        print(kw.get_attribute('name'))
+        print(kw.get_attribute('class'))
+        print(kw.get_attribute('autocomplete'))
+        print(kw.get_attribute('outerHTML'))
+
+    def is_displayed(self):
+        """判断元素是否可见"""
+        # 请求目标网址
+        url = os.path.join(os.path.abspath('html'), 'webelement.html')
+        self.driver.get(url)
+
+        # 定位元素
+        div1 = self.driver.find_element_by_id('div1')
+        div2 = self.driver.find_element_by_id('div2')
+
+        # 可见性方法
+        print(div1.is_displayed())
+        print(div2.is_displayed())
+
+    def is_enabled(self):
+        """判断元素是否可用"""
+        # 请求目标网址
+        url = os.path.join(os.path.abspath('html'), 'webelement.html')
+        self.driver.get(url)
+
+        # 定位元素
+        ipt1 = self.driver.find_element_by_xpath('//input[@name="input1"]')
+        ipt2 = self.driver.find_element_by_xpath('//input[@name="input2"]')
+
+        # 判断是否可用
+        print(ipt1.is_enabled())
+        print(ipt2.is_enabled())
+
     def quit(self, seconds=3):
         time.sleep(seconds)
         self.driver.quit()
@@ -36,6 +94,14 @@ class TestCase:
 
 if __name__ == '__main__':
     case = TestCase()
-    case.attribute()
+    # case.attribute()
+
+    # case.send_keys()
+    # case.clear()
+    # case.click()
+    # case.get_attribute()
+    # case.is_displayed()
+
+    case.is_enabled()
 
     case.quit()
