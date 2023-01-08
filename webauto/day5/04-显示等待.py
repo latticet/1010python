c5cbcaae830024b044f2ae726fccbaaa0df45c6a
latticet@gmail.com <935948757@qq.com>
2023-01-08 15:09:52 +0800

    显示等待

--- webauto/day5/04-显示等待.py
+++ webauto/day5/04-显示等待.py
@@ -8,9 +8,11 @@ driver = webdriver.Chrome()
 # 请求百度
 driver.get('http://www.baidu.com')
 
+# TODO until
 # result = WebDriverWait(driver, 5).until(EC.title_is('百度一下，你就知道1'), message='标题不一致')
+
+# TODO until_not
 # result = WebDriverWait(driver, 5).until_not(EC.title_is('百度一下，你就知道'), message='标题不一致')
 
-
-
+# TODO expected_conditions
 result = WebDriverWait(driver, 5).until(EC.title_is('百度一下，你就知道1'), message='标题不一致')
