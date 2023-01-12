094776dbcb0520b1ba5a48c6d27b378f771507e8
latticet@gmail.com <935948757@qq.com>
2023-01-12 15:36:20 +0800

    TestLoader

--- webauto/day7/10-TestLoader-discover.py
+++ webauto/day7/10-TestLoader-discover.py
@@ -2,7 +2,7 @@ from unittest import defaultTestLoader, TextTestRunner
 
 
 # 创建loader
-# 导入case目录下，以_testcase.py结尾的文件
+# 导入case目录下，以_testcase.py结尾的文件中的测试用例方法
 suite = defaultTestLoader.discover('case', pattern='*_testcase.py')
 
 # 创建runner
